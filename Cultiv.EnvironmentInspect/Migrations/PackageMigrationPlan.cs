0f7e479c4644ff2aff191ac5a2b8e2c58679402c
Sebastiaan Janssen <sebastiaan@umbraco.com>
2022-04-05 12:22:28 +0200

    Add migration to show up in Installed Packages in the Umbraco Packages section

---
+++ Cultiv.EnvironmentInspect/Migrations/PackageMigrationPlan.cs
@@ -0,0 +1,9 @@
+﻿using Umbraco.Cms.Infrastructure.Packaging;
+
+namespace Cultiv.EnvironmentInspect.Migrations
+{
+    public class PackageMigrationPlan : AutomaticPackageMigrationPlan
+    {
+        public PackageMigrationPlan() : base("Cultiv.EnvironmentInspect Dashboard") { }
+    }
+}
