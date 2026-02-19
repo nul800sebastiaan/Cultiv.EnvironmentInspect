c1d9f7f0460ead5146b9e6c85303fafd81e901f9
Jeavon Leopold <jeavon@crumpled-dog.com>
2026-02-19 12:36:20 +0000

    feat: Setting filtering and redaction

--- Cultiv.EnvironmentInspect/Services/IEnvironmentInspectService.cs
+++ Cultiv.EnvironmentInspect/Services/IEnvironmentInspectService.cs
@@ -10,4 +10,7 @@ public class EnvironmentVariable
     public required string Key { get; set; }
     public string? Value { get; set; }
     public string? Provider { get; set; }
+    public string? ProviderType { get; set; }
+    public string? ProviderSource { get; set; }
+    public string? RedactedMode { get; set; }
 }
